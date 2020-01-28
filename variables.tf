eef1c3d6128c54a611912b9e482f429e40686d5d
Chad Crowell <chad.crowell@linuxacademy.com>
2020-01-28 11:44:48 -0600

    added vars

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "client_secret" {
+ type = string
+}
+variable "region" {
+ type = string
+ default = "westus2"
+}
+variable "namespace" {
+ type = string
+ default = "ballroominaction"
+} 
